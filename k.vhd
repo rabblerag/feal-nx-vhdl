c3144b5744e1da21da6fa1b538e89b41c7d1e086
Nikolaos Giaples <giaplesnikolaos@gmail.com>
2026-03-06 01:14:15 +0200

    Removed redundant files, pipelined key scheduling based on control module signals.

--- k.vhd
+++ k.vhd
@@ -5,18 +5,13 @@ entity k is
 generic (N : integer := 32);
 port(
 k: in std_logic_vector(127 downto 0);
-clk: in std_logic;
-rst: in std_logic;
--- TO BE REMOVED ---------------------------
-k_even: out std_logic_vector(15 downto 0);
-k_odd: out std_logic_vector(15 downto 0);
---------------------------------------------
-extended_k: out std_logic_vector(0 to (N+8)*16 - 1);
-ready: out std_logic
+counter: in natural range 1 to N/2+4;
+clk, rst, enable: in std_logic;
+extended_k: out std_logic_vector(0 to (N+8)*16 - 1)
 );
 end k;
 
-architecture easy of k is
+architecture rtl of k is
   component fk is
   port(
   a: in std_logic_vector(31 downto 0);
@@ -24,60 +19,56 @@ architecture easy of k is
   f:  out std_logic_vector(31 downto 0)
   );
   end component;
-
-  signal q_sel: natural range 0 to 2 := 0;
-  signal counter: natural range 1 to N/2+5 := 1;
   
-  signal a_reg, b_reg, d_reg, q, xor_1, xor_2, fk_out: std_logic_vector(31 downto 0);
+  signal a_reg, b_reg, d_reg, a_in, b_in, d_in, q, xor_out, fk_out: std_logic_vector(31 downto 0);
+  signal extended_key_reg : std_logic_vector(0 to (N+8)*16-1);
     
   begin
 
-    xor_1 <= d_reg xor b_reg;
-    xor_2 <= xor_1 xor q;
+    xor_out <= (d_reg xor b_reg) xor q;
 
-    with q_sel select
-      q <= k(63 downto 32) xor k(31 downto 0) when 0,
-           k(63 downto 32)                    when 1,
+    with (counter mod 3) select
+      q <= k(63 downto 32) xor k(31 downto 0) when 1,
+           k(63 downto 32)                    when 2,
            k(31 downto 0)                     when others;
+
+    with counter select
+      a_in <= k(127 downto 96) when 1,
+              b_reg when others;
+    
+    with counter select
+      b_in <= k(95 downto 64) when 1,
+              fk_out when others;
+    
+    with counter select
+      d_in <= (others => '0') when 1,
+              a_reg when others;
     
     
 
-U0: fk port map (a => a_reg , b => xor_2 , f => fk_out);
+U0: fk port map (a => a_reg , b => xor_out , f => fk_out);
 
-    k_even <= fk_out(31 downto 16);
-    k_odd  <= fk_out(15 downto 0);
-
-    k_r: process(clk, rst)
+k_r: process(clk, rst)
     
-    begin
-    if rst = '1' then
-      a_reg <= k(127 downto 96);
-      b_reg <= k(95 downto 64);
-      d_reg <= (others => '0'); 
-      q_sel <= 0;
-      counter <= 1;
-      ready <= '0';
-    
-    elsif rising_edge(clk) then
-        if (counter = N/2+5) then 
-          ready <= '1';
-        else
-          if(q_sel = 2) then q_sel <= 0;
-          else q_sel <= q_sel + 1;
-          end if;
+begin
+  if rst = '1' then
+    a_reg <= (others => '0');
+    b_reg <= (others => '0');
+    d_reg <= (others => '0');
+    extended_key_reg <= (others => '0');
+  
+  elsif rising_edge(clk) and enable = '1' then
+      b_reg <= b_in;
+      a_reg <= a_in;
+      d_reg <= d_in;
 
-          b_reg <= fk_out;
-          a_reg <= b_reg;
-          d_reg <= a_reg;
-          counter <= counter + 1;
+      extended_key_reg <= extended_key_reg(0 to (N+6)*16-1) & fk_out;
 
-          extended_k(32*(counter - 1) to 32*(counter) - 17) <= fk_out(31 downto 16);
-          extended_k(32*counter - 16 to 32*(counter) - 1) <= fk_out(15 downto 0);
-          
-        end if;        
-    end if;
-    end process;
-    end easy;
+  end if;
+end process;
+
+extended_k <= extended_key_reg;
+end rtl;
 
 
 -- add_force {/k/k} -radix hex {0123456789abcdef0123456789abcdef 0ns}
